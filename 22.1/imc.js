1b7831465a15d67044f715b3d5e24ac7f0f1b47d
Luskalves <lusca.alves32@gmail.com>
2022-06-06 19:25:01 -0300

    exercicios 3,4,5 e 6 feitos

--- 22.1/imc.js
+++ 22.1/imc.js
@@ -2,6 +2,22 @@ const readline = require('readline-sync');
 
 const altura = readline.questionFloat('Digite a dua altura: ');
 const peso = readline.questionFloat('Digite o seu peso: ');
-const imc = peso / altura;
+const imc = peso / (altura**2);
+let resultado = ''
+
+if (imc <= 18.5) {
+  resultado = 'Abaixo do peso (magreza)'
+} else if (imc > 18.5 && imc <= 24.9) {
+  resultado = 'Peso normal'
+} else if (imc > 25.0 && imc <= 29.9) {
+  resultado = 'Acima do peso (sobrepeso)'
+} else if (imc > 30.0  && imc <= 34.9) {
+  resultado = 'Obesidade grau I'
+} else if (imc > 35.0  && imc <=39.9) {
+  resultado = 'Obesidade grau II'
+} else {
+  resultado = 'Obesidade grau III e IV'
+} 
 
 console.log(`Ò seu IMC é de: ${imc.toFixed(2)}`);
+console.log(`E seu resultado é: ${resultado}`);

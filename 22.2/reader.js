cf0b04cc066dd029951ca69a9c2db26edfe16e73
Luskalves <lusca.alves32@gmail.com>
2022-06-07 18:41:29 -0300

    teste

--- 22.2/reader.js
+++ 22.2/reader.js
@@ -6,11 +6,11 @@ async function leitor() {
     if (err) {
       console.error("tudo errado isso aqui", err);
       process.exit(1)
-    }
+    };
     const response = JSON.parse(data);
     response.forEach((personagem) => {
       console.log(`${personagem.id} - ${personagem.name}`);
-    })
+    });
   })
 }
 

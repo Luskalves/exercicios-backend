b607485dc5277b981470cb921d7821a3d9eedfe3
Luskalves <lusca.alves32@gmail.com>
2022-06-07 15:34:51 -0300

    exercício 1 concluido

---
+++ 22.2/promise.js
@@ -0,0 +1,16 @@
+function promessa(n1, n2, n3) {
+  let testErr = false;
+  [n1,n2,n3].forEach((n) => {
+    if (typeof n != "number") !testErr;
+  })
+  const result = new Promise((resolve, reject) => {
+    if (testErr) reject('Informe apenas números');
+
+    const response = (n1 + n2) * n3;
+    if (response < 50) reject(Error('Valor muito baixo'))
+    resolve(response);
+  })
+  return result;
+}
+
+promessa(1,200,3).then(response => console.log(response));

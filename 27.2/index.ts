9ada822ff3823258aeed9fa66b4f1fca115c9d67
Luskalves <lusca.alves32@gmail.com>
2022-08-03 15:55:18 -0300

    exercício 1 concluido

--- 27.2/index.ts
+++ 27.2/index.ts
@@ -18,3 +18,9 @@ class Person {
     return this._birthDate;
   }
 }
+
+const p1 = new Person('luska',new Date('Nov 03, 2000 15:00:00'))
+const p2 = new Person('sergio',new Date('jan 12, 1988 15:00:00'))
+
+console.log(p1.birthDate)
+console.log(p2.birthDate)
